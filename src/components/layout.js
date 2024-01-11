6e2d4c3c1717b67d93ba563a530041018428ea44
Kaloyan Angelov <64629762+kaloyaqn@users.noreply.github.com>
2024-01-11 11:11:25 +0200

    feat: added vercel analytics

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,36 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import { supabase } from '../lib/supabase';
-import Header from './Navigation/Header';
-import Footer from './Navigation/Footer';
-import toast, { Toaster } from 'react-hot-toast';
+import React, { useEffect, useState } from "react";
+import { supabase } from "../lib/supabase";
+import Header from "./Navigation/Header";
+import Footer from "./Navigation/Footer";
+import toast, { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/react";
 
-
-const Layout =({children}) =>{
-    // const [session, setSession] = useState([]);
-
-    // async function VzemameUser() {
-    //     const {
-    //       data: { user },
-    //     } = await supabase.auth.getUser();
-    //     setSession(user);
-    //     if (user) {
-    //       console.log("ima sesiq da ");
-    //     } else {
-    //       console.log("cent nqma sesiq");
-    //     }
-    //   }
-    
-    //   useEffect(() => {
-    //     VzemameUser();
-    //   }, [children]);
-    return(
-        <>
-        <Toaster />
-        <Header />
-        <main>{children}</main>
-        <Footer />
-        </>
-    )
-}
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Toaster />
+      <Header />
+      <main>
+        {children}
+        <Analytics />
+      </main>
+      <Footer />
+    </>
+  );
+};
 
 export default Layout;
