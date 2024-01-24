9a1edab4b74eb49ffdb0348b4b2303b8d3aaa2d3
kaloyaqn <kallnoob11@gmail.com>
2024-01-24 16:01:51 +0200

    hotfix: fixed speenInsights not building

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,7 @@ import Header from "./Navigation/Header";
 import Footer from "./Navigation/Footer";
 import toast, { Toaster } from "react-hot-toast";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const Layout = ({ children, session }) => {
   return (
